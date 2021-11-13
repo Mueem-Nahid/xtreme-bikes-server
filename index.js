aac9b0d1a2c3a5019d54551f8d0e0380041f7642
Mueem-Nahid <mueem51@gmail.com>
2021-11-13 23:38:12 +0600

    code updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
@@ -13,7 +13,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.seewk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+console.log(uri);
 async function run() {
     try {
         await client.connect();
